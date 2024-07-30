bae631ebd1f39567897d51e132dacd07e910e0c5
jeanfrancoisgratton <jean-francois@famillegratton.net>
2024-07-30 17:57:55 -0400

    Ready to test unseal

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # hcpVaultLib
+___
 
-Hashicorp Vault client Lib
+A Hashicorp Vault client package
+
+This lib (package) was first written for my own use and was not even published on Github at first.
+As I intend on expanding the package, now is a good time to publish and document the package.
+
+
+## What's in there:
+
+Mainly functions to:
+- unseal a Vault
+- login with a token
+- login with the userpass method
+- fetch a secret from a given secret engine
--- unseal.go
+++ unseal.go
@@ -9,44 +9,50 @@ import (
 	"encoding/json"
 	"fmt"
 	vault "github.com/hashicorp/vault/api"
-	"log"
+	cerr "github.com/jeanfrancoisgratton/customError"
+	hf "github.com/jeanfrancoisgratton/helperFunctions"
 	"os"
 )
 
-type UnsealKey struct {
+type VaultOperatorInfo struct {
 	Keys []string `json:"keys"`
 }
 
-func UnsealVault(client *vault.Client, jsonFilePath string, requiredKeys int) error {
+func UnsealVault(client *vault.Client, jsonFilePath string, requiredKeys int) *cerr.CustomError {
 	// Read the JSON file
 	fileContent, err := os.ReadFile(jsonFilePath)
 	if err != nil {
-		return fmt.Errorf("failed to read JSON file: %w", err)
+		return &cerr.CustomError{Title: "Failed to read JSON file", Message: err.Error()}
 	}
 
 	// Decrypt the fields if needed
 	// You can add your decryption logic here
 
 	// Parse the JSON content
-	var unsealKeys UnsealKey
+	var unsealKeys VaultOperatorInfo
 	if err := json.Unmarshal(fileContent, &unsealKeys); err != nil {
-		return fmt.Errorf("failed to parse JSON file: %w", err)
+		return &cerr.CustomError{Title: "Failed to parse JSON file", Message: err.Error()}
 	}
 
 	// Unseal the Vault
 	for i, key := range unsealKeys.Keys {
+		key := hf.DecodeString(key, "")
 		if i >= requiredKeys {
 			break
 		}
 		resp, err := client.Sys().Unseal(key)
+		// the return statement is commented for now: do we really need to return if a key part is wrong ? We might need a boolean to deal with this
+		// something like var analSecurity bool, where if true we uncomment the lines below.
+		// otherwise: we keep the loop going, it'll fail if all key parts have been processed, anyway
 		if err != nil {
-			return fmt.Errorf("failed to unseal vault with key part %d: %w", i+1, err)
+			//return &cerr.CustomError{Fatality: cerr.Continuable, Title: fmt.Sprintf("Failed to unseal vault with key part %d", i+1), Message: err.Error()}
+			continue // <-- for now.. Remove this line if we use that boolean var I've mentioned, and uncomment the above
 		}
 		if resp.Sealed == false {
-			log.Println("Vault is successfully unsealed")
+			fmt.Printf("%s\n", hf.Green("Vault is unsealed"))
 			return nil
 		}
 	}
 
-	return fmt.Errorf("failed to unseal the Vault with the provided key parts")
+	return &cerr.CustomError{Title: "failed to unseal the Vault with the provided key parts"}
 }
